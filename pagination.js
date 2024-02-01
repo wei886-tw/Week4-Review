da8774d85b52711ec0bc2d70529254cfe6eeb660
wei886-tw <lunhowl@gmail.com>
2024-02-01 18:15:57 +0800

    2nd commit

--- pagination.js
+++ pagination.js
@@ -1,15 +1,15 @@
 export default {
-  props: ['pages', 'getProducts()'],
-  template: `<nav aria-label="Page navigation example">
+  props: ['pages', 'getProducts'],
+  template: `     <nav aria-label="Page navigation example">
   <ul class="pagination">
-      <li class="page-item" >
-          {{pages}}
-          <a class="page-link" href="#" aria-label="Previous" >
+      <li class="page-item" :class="{disabled: pages.current_page === 1}">
+          <a class="page-link" href="#" aria-label="Previous">
               <span aria-hidden="true">&laquo;</span>
           </a>
       </li>
-      <li class="page-item" v-for="page in pages.total_pages"><a class="page-link" href="#" @click="getProducts(page = page)">{{page}}</a></li>
-      <li class="page-item" :class="{}">
+      <li class="page-item" v-for="(page, index) in pages.total_pages" :key="index +123"><a
+              class="page-link" href="#" @click="getProducts(page = page)">{{page}}</a></li>
+      <li class="page-item" :class="{disabled: !pages.has_next}">
           <a class="page-link" href="#" aria-label="Next">
               <span aria-hidden="true">&raquo;</span>
           </a>
